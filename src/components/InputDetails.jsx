1990e8ba3d594e734aa16231105ca4755609fa28
Faiz Hameed <faizftb@gmail.com>
2020-11-24 19:27:43 +0530

    added generate Lin

--- src/components/InputDetails.jsx
+++ src/components/InputDetails.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
-const InputDetails = () => {
+const InputDetails = ({ generateLink }) => {
   const dtStart = React.useRef();
   const dtEnd = React.useRef();
   const descRef = React.useRef();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(
-      dtStart.current.value,
-      dtEnd.current.value,
-      descRef.current.value
-    );
-  };
+
   return (
     <div>
       <form>
         <input name="eventStart" type="datetime-local" ref={dtStart} />
         <input name="eventEnd" type="datetime-local" ref={dtEnd} />
         <input type="text" name="description" ref={descRef} />
-        <input type="submit" value="Generate Link" onClick={handleSubmit} />
+        <input
+          type="submit"
+          value="Generate Link"
+          onClick={(e) => {
+            e.preventDefault();
+            generateLink({
+              startDate: dtStart.current.value,
+              endDate: dtEnd.current.value,
+              description: descRef.current.value,
+            });
+          }}
+        />
       </form>
     </div>
   );
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import InputDetails from "../components/InputDetails";
 
 const Main = ({ children }) => {
+  const [state, setState] = useState({
+    data: "",
+    googleLink: "",
+  });
+  const generateLink = (data) => {
+    setState({ data });
+    data.description.replace(" ", "+");
+    setState({
+      googleLink: `https://www.google.com/calendar/render?action=TEMPLATE&text=Your+Event+Name&dates=${data.startDate}/${data.endDate}&details=${data.description}&location=Waldorf+Astoria,+301+Park+Ave+,+New+York,+NY+10022&sf=true&output=xml`,
+    });
+  };
   return (
     <div>
       <Header />
-      <InputDetails />
+      <InputDetails generateLink={generateLink} />
       <div>{children}</div>
+      <p>Google Link</p>
+      <div>
+        <p>{state.googleLink}</p>
+      </div>
     </div>
   );
 };
