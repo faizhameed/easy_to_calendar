090c492ec9fa20e5c9331fb4bf5522304ade2a90
Faiz Hameed <faizftb@gmail.com>
2020-12-03 19:16:46 +0530

    context api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
 import Content from "./components/Content";
+import ThemeContext from "./context/theme-context";
 import Main from "./layout/Main";
 
 function App() {
   return (
-    <div className="App">
-      <Main>
-        <Content />
-      </Main>
-    </div>
+    <ThemeContext.Consumer>
+      <div className="App">
+        <Main>
+          <Content />
+        </Main>
+      </div>
+    </ThemeContext.Consumer>
   );
 }
 
